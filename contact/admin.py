2ee2e9748b962f94875ced94fa568a7347d5e1b8
muslimbek77 <muslimjonbekali@mail.ru>
2024-09-09 17:42:29 +0500

    add contact model register admin.py

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Contact
 
-# Register your models here.
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('user__username','user__phone_number','short_message')
+    search_fields = ('user__username', 'message')
+
+    def short_message(self,obj):
+        return obj.message[:20]+'...'
