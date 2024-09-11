9c8ef6c837bc48ad38f62135d7ea88fe9192a133
label0101 <amalfayzullayev908@gmail.com>
2024-09-11 17:12:11 +0500

    card admin o'zgardi

--- cart/admin.py
+++ cart/admin.py
@@ -3,6 +3,5 @@ from .models import Cart
 
 @admin.register(Cart)
 class CartAdmin(admin.ModelAdmin):
-    list_display = ('user', 'product')
+    list_display = ('user', 'product') 
     list_filter = ('is_active')
-#Amal
