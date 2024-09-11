f24a1ab2e3ea1d3dbe5135223fa7771f13184336
asilbek-ismoilov <asilbe.m00d@gmail.com>
2024-09-11 09:33:05 +0300

    Cart

--- cart/admin.py
+++ cart/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
 from .models import Cart
 
-admin.site.register((Cart))
+@admin.register(Cart)
+class CartAdmin(admin.ModelAdmin):
+    list_display = ('user', 'product') 
