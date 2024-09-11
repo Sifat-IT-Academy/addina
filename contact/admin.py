d541d4050070fa1f07f075cc1f69032d4ea8ed1f
Ocak14 <sayfullayeva191@gmail.com>
2024-09-11 17:00:26 +0500

    add

--- contact/admin.py
+++ contact/admin.py
@@ -1,11 +1,10 @@
 from django.contrib import admin
-from .models import Contact
+from contact.models import Contact
 
-#Aminjon
 @admin.register(Contact)
 class ContactAdmin(admin.ModelAdmin):
-    list_display = ('user__username','user__phone_number','short_message')
-    search_fields = ('user__username', 'message')
+    list_display = ['user','short_message']
+    search_fields = ['user', 'message']
 
     def short_message(self,obj):
         return obj.message[:20]+'...'
