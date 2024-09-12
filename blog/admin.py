8cec07b0f3c14220148ee5c601af128bb21a150f
Ocak14 <sayfullayeva191@gmail.com>
2024-09-12 12:51:56 +0500

    add blog/admin.py

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from django.shortcuts import render
+from .models import Blog,BlogCategory,BlogComment
+from django.utils.html import format_html
 
-# Register your models here.
+@admin.register(Blog)
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ("image","title","description","user","created_date","category")
+    readonly_fields = ['id']
+
+    def img(self, obj):
+         return format_html('<img width="100" height="100" src="{}"style="border-radius: 50%;" />'.format(obj.image.url))
+@admin.register(BlogCategory)
+class BlogCategoryAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    readonly_fields = ['id']
+
+    def img(self, obj):
+         return format_html('<img width="100" height="100" src="{}"style="border-radius: 50%;" />'.format(obj.image.url))
+  
+
+
+@admin.register(BlogComment)
+class BlogCommentAdmin(admin.ModelAdmin):
+    list_display = ("user","comment","created_date","user","created_date","blog")
+    readonly_fields = ['id']
+
+    def img(self, obj):
+         return format_html('<img width="100" height="100" src="{}"style="border-radius: 50%;" />'.format(obj.image.url))
+  
