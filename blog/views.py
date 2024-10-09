bfeaf73795cfd12dbac6e70725afd95a461363fe
Samandar <msamandar201@gmail.com>
2024-10-09 15:01:14 +0500

    all settings

--- blog/views.py
+++ blog/views.py
@@ -1,22 +1,15 @@
-<<<<<<< HEAD
 from django.shortcuts import render, redirect, get_object_or_404
-from .models import Blog,BlogCategory,BlogComment,BlogTag
 from django.db.models import Count
 from .forms import CommentForm, ReplyForm
 from django.views.generic import ListView
-from django.db.models import Count
 from django.views.generic import DetailView
-=======
-from django.shortcuts import render
-from django.views.generic import ListView
-from .models import Blog,BlogCategory,BlogComment,BlogTag,Blog_details
->>>>>>> ffa80903c789f0f2e8c562fbbadaa7aa4cd6a0a2
+from .models import *
+
 
 class BlogListView(ListView):
     model = Blog
     context_object_name = "blogs"
     template_name = "blog.html"
-<<<<<<< HEAD
     paginate_by = 3
 
     def get_queryset(self):
@@ -31,7 +24,6 @@ class BlogListView(ListView):
             return Blog.objects.filter(category__id=category_id)
         if q:
             return Blog.objects.filter(title__icontains=q)
-
         return Blog.objects.all()
 
     def get_context_data(self, **kwargs):
@@ -39,13 +31,8 @@ class BlogListView(ListView):
         context['categories'] = BlogCategory.objects.annotate(blog_count=Count('blog'))
         context['tags'] = BlogTag.objects.annotate(blog_count=Count('blogs'))
         context['all_blogs_count'] = Blog.objects.count()
-
-        # Add comments
         context['comments'] = BlogComment.objects.select_related('blog').all()
-
-        # Add the latest 3 blogs
         context['recent_blogs'] = Blog.objects.order_by('-created_date').all()[:3]
-
         if 'category_id' in self.kwargs:
             context['current_category'] = BlogCategory.objects.get(id=self.kwargs['category_id'])
         if 'tag_id' in self.kwargs:
@@ -135,11 +122,10 @@ class BlogDetailView(DetailView):
             context['comment_form'] = form
             context['reply_form'] = reply_form
             return self.render_to_response(context)
-=======
     paginate_by = 2 
     
 class Blog_detailsListView(ListView):
     model = Blog_details
     context_object_name = 'blog_details'
     template_name = 'blog-details.html'
->>>>>>> ffa80903c789f0f2e8c562fbbadaa7aa4cd6a0a2
+
--- portfolio/views.py
+++ portfolio/views.py
@@ -1,10 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import *
 from django.views.generic import ListView
-from .models import Portfolio,Portfolio_details
+from .models import *
 
 class PortfolioListView(ListView):
     model = Portfolio
-<<<<<<< HEAD
     template_name = 'portfolio.html'
     context_object_name = 'portfolio'
     paginate_by = 3  # Adjust pagination if needed
@@ -17,8 +16,7 @@ class PortfolioListView(ListView):
 
 def portfolio_list_view(request):
     portfolios = Portfolio.objects.all()
-    paginator = Paginator(portfolios, 3)
-
+    paginator = 3
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
 
@@ -28,13 +26,11 @@ def portfolio_list_view(request):
 def portfolio_detail(request, portfolio_id):
     portfolio = get_object_or_404(Portfolio, id=portfolio_id)
     return render(request, 'portfolio-details.html', {'portfolio': portfolio})
-
-=======
     context_object_name = "portfolio"
     template_name = "portfolio.html"
 class PortfolioDetailsListView(ListView):
     model = Portfolio_details
     context_object_name = "portfolios"
     template_name = "portfolio-details.html"
->>>>>>> ffa80903c789f0f2e8c562fbbadaa7aa4cd6a0a2
+
 
